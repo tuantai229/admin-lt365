c93fb675ce1372512c9f2dc339b1b46975599f9c
Trịnh Tuấn Tài <tuantai229@icloud.com>
2025-07-18 11:20:17 +0700

    Create school action in list

--- app/Filament/Resources/SchoolResource/Pages/ListSchools.php
+++ app/Filament/Resources/SchoolResource/Pages/ListSchools.php
@@ -9,4 +9,11 @@ use Filament\Resources\Pages\ListRecords;
 class ListSchools extends ListRecords
 {
     protected static string $resource = SchoolResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
