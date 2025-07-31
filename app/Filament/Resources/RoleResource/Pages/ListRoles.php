11af07adf33f6dd8874f49d955390c1d9f61e2ff
Trịnh Tuấn Tài <tuantai229@icloud.com>
2025-07-31 11:25:45 +0700

    Add create action to ListRoles header actions
    Introduced the getHeaderActions method in ListRoles to include a CreateAction, enabling users to create new roles directly from the roles list page.

--- app/Filament/Resources/RoleResource/Pages/ListRoles.php
+++ app/Filament/Resources/RoleResource/Pages/ListRoles.php
@@ -9,4 +9,11 @@ use Filament\Resources\Pages\ListRecords;
 class ListRoles extends ListRecords
 {
     protected static string $resource = RoleResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
